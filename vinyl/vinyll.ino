06fcfd5fe5eeff859969ceb49fc5081a1857b64b
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2025-02-19 16:27:14 +0100

    vinyl.ino

---
+++ vinyl/vinyll.ino
@@ -0,0 +1,19 @@
+#include <Audio.h>
+#include "Vinyl.h"
+
+Vinyl vinyl;
+AudioOutputI2S out;
+AudioControlSGTL5000 audioShield;
+AudioConnection patchCord0(vinyl,0,out,0);
+AudioConnection patchCord1(vinyl,0,out,1);
+
+void setup() {
+  AudioMemory(2);
+  audioShield.enable();
+  audioShield.volume(0.1);
+}
+
+void loop() {
+  vinyl.setParamValue("freq",random(50,1000));
+  delay(50);
+}
