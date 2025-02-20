7941a3c7cb735a4f002caae09d2fcf4b232fa681
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2025-02-20 09:26:11 +0100

    vinyl.ino : add control functions using midiusb

--- vinyl/vinyl.ino
+++ vinyl/vinyl.ino
@@ -1,29 +1,72 @@
 #include <Audio.h>
-#include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
+#include <SerialFlash.h>
+#include <usbMIDI.h>
+#include "Vinyl.h"
 
-// 定义音频模块的连接
-AudioPlaySdWav player;  // 用于播放WAV文件
-AudioOutputI2S i2s;     // 使用I2S输出音频
-AudioConnection patchCord0(player, 0, i2s, 0);  // 连接播放器到音频输出
-AudioControlSGTL5000 audioShield;  // 控制音频模块
+#define SDCARD_CS_PIN 10
+#define SDCARD_MOSI_PIN  11
+#define SDCARD_SCK_PIN   13
+#define PLAY_BUTTON_PIN 0/2/3/4/5
+
+Vinyl vinyl;
+AudioOutputI2S out;
+AudioPlaySdWav audioSD;
+AudioControlSGTL5000 audioShield;
+AudioMixer4 mix;
+
+AudioConnection patchCord0(vinyl, 0, mix, 0);
+AudioConnection patchCord1(audioSD, 0, mix, 1);
+AudioConnection patchCord2;
+AudioConnection patchCord3;
 
 void setup() {
-  // 初始化音频模块
   audioShield.enable();
-  audioShield.inputSelect(AUDIO_INPUT_LINEIN);  // 设置输入源为线路输入
-  audioShield.volume(0.5);  // 设置音量（0.0到1.0）
+  audioShield.volume(2);
+  AudioMemory(4);
+  pinMode(PLAY_BUTTON_PIN, INPUT);
+  mix.gain(0, 5);
+  mix.gain(1, 0.15);
 
-  // 初始化SD卡
-  if (!SD.begin()) {
-    while (1);  // 如果SD卡初始化失败，进入死循环
+  SPI.setMOSI(SDCARD_MOSI_PIN);
+  SPI.setSCK(SDCARD_SCK_PIN);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    while (1) {
+      delay(500);
+    }
   }
-
-  // 播放WAV文件
-  player.play("yintian.wav");
 }
 
 void loop() {
-  // 主循环为空，音频播放由Audio Library自动处理
+  int knobValue = analogRead(A4);
+  int playButtonState = digitalRead(PLAY_BUTTON_PIN);
+  float gain = map(knobValue, 0, 1023, 0, 7);
+  mix.gain(0, gain);
+
+  static float previousGainValue = -1.0;
+  float gainValue = knobValue / 102.3;
+  if (fabs(gainValue - previousGainValue) > 0.1) {
+    usbMIDI.sendControlChange(7, gainValue * 127 / 7, 1); // CC 7: Volume
+    previousGainValue = gainValue;
+  }
+
+  if (playButtonState && audioSD.isPlaying() == false) {
+    usbMIDI.sendNoteOn(60, 127, 1); // MIDI Note On
+    delay(800);
+    audioSD.play("song.WAV");
+    patchCord2.connect(mix, 0, out, 0);
+    patchCord3.connect(mix, 0, out, 1);
+    delay(500);
+  } else if (playButtonState && audioSD.isPlaying() == true) {
+    usbMIDI.sendNoteOff(60, 0, 1); // MIDI Note Off
+    delay(100);
+    audioSD.stop();
+    patchCord2.disconnect();
+    patchCord3.disconnect();
+    delay(500);
+  }
+
+  usbMIDI.read(); 
+  delay(100);
 }
