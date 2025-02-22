2202447d92cec4e1aa6da8148d9cbf3f217144bf
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2025-02-22 20:04:58 +0100

    ino

--- vinyl/vinyl.ino
+++ vinyl/vinyl.ino
@@ -137,62 +137,188 @@
 
 // 删除vinyl相关后：成功播放音乐
 
+// #include <Audio.h>
+// #include <SPI.h>
+// #include <SD.h>
+// #include <SerialFlash.h>
+
+// #define SDCARD_CS_PIN 10
+// #define SDCARD_MOSI_PIN  11
+// #define SDCARD_SCK_PIN   13
+
+// AudioOutputI2S out;
+// AudioPlaySdWav audioSD;
+// AudioControlSGTL5000 audioShield;
+// AudioMixer4 mix;
+
+// AudioConnection patchCord0(audioSD, 0, mix, 0);
+// AudioConnection patchCord1(mix, 0, out, 0);
+// AudioConnection patchCord2(mix, 0, out, 1);
+
+// void setup() {
+//   Serial.begin(9600);
+
+//   audioShield.enable();
+//   audioShield.volume(2);
+//   AudioMemory(4);
+//   mix.gain(0, 0.5);
+
+//   SPI.setMOSI(SDCARD_MOSI_PIN);
+//   SPI.setSCK(SDCARD_SCK_PIN);
+  
+//   Serial.println("Checking SD Card...");
+//   if (!SD.begin(SDCARD_CS_PIN)) {
+//     Serial.println("SD Card initialization failed!");
+//     while (1) {
+//       delay(500);
+//     }
+//   }
+//   Serial.println("SD Card OK.");
+
+//   if (!SD.exists("YINTIAN.WAV")) {
+//     Serial.println("ERROR: YINTIAN.WAV NOT FOUND!");
+//     while (1) {
+//       delay(500);
+//     }
+//   }
+//   Serial.println("Found YINTIAN.WAV, starting playback.");
+  
+//   audioSD.play("YINTIAN.WAV");
+// }
+
+// void loop() {
+//   if (!audioSD.isPlaying()) {
+//     audioSD.play("YINTIAN.WAV");
+//   }
+// }
+
+
+
+
+
+//  Avec Midi Control::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 #include <Audio.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+#include <MIDI.h>  // Assurez-vous d'avoir installé la bibliothèque Arduino MIDI
+
+#include "Vinyl.h"
+#include <cmath>
 
 #define SDCARD_CS_PIN 10
-#define SDCARD_MOSI_PIN  11
-#define SDCARD_SCK_PIN   13
+#define SDCARD_MOSI_PIN 11
+#define SDCARD_SCK_PIN 13
 
+Vinyl vinyl;
 AudioOutputI2S out;
 AudioPlaySdWav audioSD;
 AudioControlSGTL5000 audioShield;
 AudioMixer4 mix;
 
-AudioConnection patchCord0(audioSD, 0, mix, 0);
-AudioConnection patchCord1(mix, 0, out, 0);
-AudioConnection patchCord2(mix, 0, out, 1);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI); // Créer un port MIDI
+
+AudioConnection patchCord0(audioSD, 0, vinyl, 0);
+AudioConnection patchCord1(vinyl, 0, mix, 0);
+AudioConnection patchCord2(mix, 0, out, 0); // Gauche
+AudioConnection patchCord3(mix, 0, out, 1); // Droite
+
+// Enregistrer les valeurs des paramètres précédents pour éviter les envois répétés
+float previousGain = -1.0;
+float previousScratch = -1.0;
+float previousDust = -1.0;
+float previousRumble = -1.0;
+float previousMotor = -1.0;
 
 void setup() {
   Serial.begin(9600);
+  MIDI.begin(MIDI_CHANNEL_OMNI); // Écouter tous les canaux MIDI
+  MIDI.setHandleControlChange(handleControlChange); // Écouter les événements CC
 
   audioShield.enable();
-  audioShield.volume(2);
-  AudioMemory(4);
-  mix.gain(0, 0.5);
+  audioShield.volume(2); // Définir le volume du bouclier audio
+  AudioMemory(20); // Allouer de la mémoire audio
+  mix.gain(0, 1);  // Définir le gain de l'entrée 0 (bruit)
+  mix.gain(1, 0);  // Définir le gain de l'entrée 1 (audio SD)
 
   SPI.setMOSI(SDCARD_MOSI_PIN);
   SPI.setSCK(SDCARD_SCK_PIN);
-  
-  Serial.println("Checking SD Card...");
-  if (!SD.begin(SDCARD_CS_PIN)) {
-    Serial.println("SD Card initialization failed!");
+  if (!(SD.begin(SDCARD_CS_PIN))) {
     while (1) {
+      Serial.println("Impossible d'accéder à la carte SD");
       delay(500);
     }
   }
-  Serial.println("SD Card OK.");
 
-  if (!SD.exists("YINTIAN.WAV")) {
-    Serial.println("ERROR: YINTIAN.WAV NOT FOUND!");
-    while (1) {
-      delay(500);
-    }
-  }
-  Serial.println("Found YINTIAN.WAV, starting playback.");
-  
+  // Jouer de la musique
   audioSD.play("YINTIAN.WAV");
 }
 
 void loop() {
+  MIDI.read(); // Écouter les messages MIDI
+
+  // Si la lecture de la musique se termine, relancer automatiquement
   if (!audioSD.isPlaying()) {
     audioSD.play("YINTIAN.WAV");
   }
+
+  delay(100); // Éviter une exécution trop rapide
 }
 
+// Traiter les messages de contrôle MIDI
+void handleControlChange(byte channel, byte controller, byte value) {
+  float newValue = value / 127.0; // Mappage de MIDI 0-127 à 0.0 - 1.0
+  
+  switch (controller) {
+    case 7: // CC7 contrôle le volume général (gain)
+      if (fabs(newValue - previousGain) > 0.01) {
+        vinyl.setParamValue("gain", newValue);
+        previousGain = newValue;
+        Serial.print("Gain: ");
+        Serial.println(newValue);
+      }
+      break;
+      
+    case 10: // CC10 contrôle le son de scratch
+      if (fabs(newValue - previousScratch) > 0.01) {
+        vinyl.setParamValue("Scratch Trigger", newValue);
+        previousScratch = newValue;
+        Serial.print("Scratch: ");
+        Serial.println(newValue);
+      }
+      break;
 
+    case 11: // CC11 contrôle le son de poussière
+      if (fabs(newValue - previousDust) > 0.01) {
+        vinyl.setParamValue("Pop Trigger", newValue);
+        previousDust = newValue;
+        Serial.print("Poussière: ");
+        Serial.println(newValue);
+      }
+      break;
 
+    case 12: // CC12 contrôle le déclencheur de ronflement
+      if (fabs(newValue - previousRumble) > 0.01) {
+        vinyl.setParamValue("Rumble Trigger", newValue);
+        previousRumble = newValue;
+        Serial.print("Ronflement: ");
+        Serial.println(newValue);
+      }
+      break;
 
+    case 13: // CC13 contrôle le déclencheur de bruit moteur
+      if (fabs(newValue - previousMotor) > 0.01) {
+        vinyl.setParamValue("Motor Noise Trigger", newValue);
+        previousMotor = newValue;
+        Serial.print("Bruit moteur: ");
+        Serial.println(newValue);
+      }
+      break;
 
+    default:
+      Serial.print("MIDI CC non géré: ");
+      Serial.println(controller);
+      break;
+  }
+}
